aac6daa9bfad8a2145fc96bc6a87155735bb766d
pixelcraftondemand-dev <pixelcraftondemand@gmail.com>
2026-06-11 15:00:11 +0000

    patch 9

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        unoptimized: true
-    }
+    images: {
+        unoptimized: true,
+    },
+    experimental: {
+        nodeMiddleware: true,
+    },
 };
 
 export default nextConfig;
